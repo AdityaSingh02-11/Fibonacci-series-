eae8d6ad94704511f696874af3d4a4e7590f531d
AdityaSingh02-11 <adityasinghchotu630@gmail.com>
2025-11-12 22:45:56 +0530

    Create Fibo.py

---
+++ Fibo.py
@@ -0,0 +1,12 @@
+# Fibonacci Series in Python
+
+n = int(input("Enter the number of terms: "))
+
+a, b = 0, 1
+print("Fibonacci Series:", a, b, end=" ")
+
+for i in range(2, n):
+    c = a + b
+    print(c, end=" ")
+    a = b
+    b = c
